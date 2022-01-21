c8c4aa9121eeb95a52b5831324c60cf11fe07826
marekbeckmann <marekbeckmann@bektroid-studios.de>
2022-01-21 12:22:01 +0100

    Added Info

--- check-eol.py
+++ check-eol.py
@@ -116,6 +116,7 @@ try:
 except:
     message['summary'] += "\nOS: Unknown | add --name"
     message['summary'] += "\nHomepage: Unknown | add --web"
+message['summary'] += "More lifecyle info: https://endoflife.date/" + distribution
 print("{summary}".format(
     summary=message.get('summary'),
 ))
